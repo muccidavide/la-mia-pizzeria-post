e0111c013a10daeee708961068c6b18aeeeb5e4d
muccidavide <davidemucci0804@gmail.com>
2022-10-05 15:47:24 +0200

    Complete no bonus yet

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -1,78 +1,79 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Design;
 using la_mia_pizzeria_static.Data;
+using Microsoft.IdentityModel.Tokens;
 
 namespace la_mia_pizzeria_post.Controllers
 {
     public class PizzaController : Controller
     {
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formPizza);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza pizzaToCreate = new Pizza();
+                pizzaToCreate.Name = formPizza.Name;
+                pizzaToCreate.Description = formPizza.Description;
+                pizzaToCreate.Image = formPizza.Image;
+                pizzaToCreate.Price = formPizza.Price;
+
+                db.Pizzas.Add(pizzaToCreate);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+
         [HttpGet]
         public IActionResult Index()
         {
 
             /// FAKE DB
-            Pizza pizzaMargherita = new("margherita", "pizza margherita", "/img/img-1.jpg", 13.30f);
-            Pizza pizzaNapoli = new("napoli", "pizza napoli", "/img/img-2.jpeg", 10.00f);
-            Pizza pizzaBufala = new("bufala", "pizza bufala", "/img/img-3.jpeg", 15.50f);
-            Pizza pizzaMarinara = new("marinara", "pizza marinara", "/img/img-4.jpg", 12.30f);
-            Pizza pizzaQuattroFormaggi = new("quattro formaggi", "pizza quatro formaggi", "/img/img-5.jpeg", 13.00f);
-            Pizza pizzaBianca = new("bianca", "pizza bianca", "/img/img-6.jpeg", 15.50f);
-            Pizza pizzaOliveeAcciughe = new("olive e acciughe", "pizza olive e acciughe", "/img/img-7.jpeg", 12.30f);
-            Pizza pizzaRadicchio = new("raddicchio", "pizza raddicchio", "/img/img-8.jpeg", 13.00f);
-            Pizza pizzaGenovese = new("genovese", "pizza genovese", "/img/img-9.jpg", 15.50f);
+            //Pizza pizzaMargherita = new("margherita", "pizza margherita", "/img/img-1.jpg", 13.30f);
+            //Pizza pizzaNapoli = new("napoli", "pizza napoli", "/img/img-2.jpeg", 10.00f);
+            //Pizza pizzaBufala = new("bufala", "pizza bufala", "/img/img-3.jpeg", 15.50f);
+            //Pizza pizzaMarinara = new("marinara", "pizza marinara", "/img/img-4.jpg", 12.30f);
+            //Pizza pizzaQuattroFormaggi = new("quattro formaggi", "pizza quatro formaggi", "/img/img-5.jpeg", 13.00f);
+            //Pizza pizzaBianca = new("bianca", "pizza bianca", "/img/img-6.jpeg", 15.50f);
+            //Pizza pizzaOliveeAcciughe = new("olive e acciughe", "pizza olive e acciughe", "/img/img-7.jpeg", 12.30f);
+            //Pizza pizzaRadicchio = new("raddicchio", "pizza raddicchio", "/img/img-8.jpeg", 13.00f);
+            //Pizza pizzaGenovese = new("genovese", "pizza genovese", "/img/img-9.jpg", 15.50f);
 
             List<Pizza> myMenu =new List<Pizza>();
 
             using (PizzaContext db = new PizzaContext())
             {
-            //    db.Add(pizzaMargherita);
-            //    db.Add(pizzaNapoli);
-            //    db.Add(pizzaBufala);
-            //    db.Add(pizzaMarinara);
-            //    db.Add(pizzaQuattroFormaggi);
-            //    db.Add(pizzaBianca);
-            //    db.Add(pizzaOliveeAcciughe);
-            //    db.Add(pizzaRadicchio);
-            //    db.Add(pizzaGenovese);
-
-            //    db.SaveChanges();
 
                myMenu = db.Pizzas.OrderBy(pizza => pizza.Name).ToList<Pizza>();
 
             }
-
-            //myMenu.Clear();
 
             return View("Index", myMenu);
         }
 
         public IActionResult Details(int id)
         {
+            Pizza pizza;
 
-            Pizza pizzaMargherita = new("margherita", "pizza margherita", "/img/img-1.jpg", 13.30f);
-            Pizza pizzaNapoli = new("napoli", "pizza napoli", "/img/img-2.jpeg", 10.00f);
-            Pizza pizzaBufala = new("bufala", "pizza bufala", "/img/img-3.jpeg", 15.50f);
-            Pizza pizzaMarinara = new("marinara", "pizza marinara", "/img/img-4.jpg", 12.30f);
-            Pizza pizzaQuattroFormaggi = new("quattro formaggi", "pizza quatro formaggi", "/img/img-5.jpeg", 13.00f);
-            Pizza pizzaBianca = new("bianca", "pizza bianca", "/img/img-6.jpeg", 15.50f);
-            Pizza pizzaOliveeAcciughe = new("olive e acciughe", "pizza olive e acciughe", "/img/img-7.jpeg", 12.30f);
-            Pizza pizzaRadicchio = new("raddicchio", "pizza raddicchio", "/img/img-8.jpeg", 13.00f);
-            Pizza pizzaGenovese = new("genovese", "pizza genovese", "/img/img-9.jpg", 15.50f);
+            using (PizzaContext db = new PizzaContext())
+            {
+                pizza = db.Pizzas.Where(dbPizza => dbPizza.PizzaId == id).First();
 
-            List<Pizza> myMenu = new List<Pizza>();
-            myMenu.Add(pizzaMargherita);
-            myMenu.Add(pizzaNapoli);
-            myMenu.Add(pizzaBufala);
-            myMenu.Add(pizzaMarinara);
-            myMenu.Add(pizzaQuattroFormaggi);
-            myMenu.Add(pizzaBianca);
-            myMenu.Add(pizzaOliveeAcciughe);
-            myMenu.Add(pizzaRadicchio);
-            myMenu.Add(pizzaGenovese);
+            }
 
-            Pizza pizza = myMenu[id];
-
-            return View("Show", myMenu[id]);
+            return View("Show", pizza);
         }
     }
 }
